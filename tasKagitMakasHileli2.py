0b0a300a03e85ed50523231d925d09d37c41355d
Fatih Murat Özen <89149671+zyprexx@users.noreply.github.com>
2024-03-03 19:37:54 +0300

    Add files via upload

---
+++ tasKagitMakasHileli2.py
@@ -0,0 +1,56 @@
+import random
+
+def bilgisayar_secimi_uret():
+    n = random.randint(1,3)
+    if n == 1:
+        return "taş"
+    elif n == 2:
+        return "kağıt"
+    else:
+        return "makas"
+    
+def hileli_bilgisayar_secimi_uret(kullanici_secimi):
+    if kullanici_secimi == "taş":
+        return "kağıt"
+    elif kullanici_secimi == "kağıt":
+        return "makas"
+    else:
+        return "taş"
+    
+skor_kullanici = 0
+skor_bilgisayar = 0
+
+while True:
+
+    kullanici_secimi = input("Taş? Kağıt? Makas?")
+    k = random.randint(1,2)
+    if k == 1:
+        bilgisayar_secimi = bilgisayar_secimi_uret()
+    else:
+        bilgisayar_secimi = hileli_bilgisayar_secimi_uret(kullanici_secimi)
+
+    print("Bilgisayar :", bilgisayar_secimi)
+
+    if bilgisayar_secimi == kullanici_secimi:
+        print("BERABERE")
+    elif bilgisayar_secimi == "taş" and kullanici_secimi == "kağıt":
+        skor_kullanici += 1
+    elif bilgisayar_secimi == "kağıt" and kullanici_secimi == "makas":
+        skor_kullanici += 1
+    elif bilgisayar_secimi == "makas" and kullanici_secimi == "taş":
+        skor_kullanici += 1
+    else:
+        skor_bilgisayar +=1
+
+    print("Siz :",skor_kullanici, "VS Bilgisayar :", skor_bilgisayar)
+
+    if skor_bilgisayar == 3 or skor_kullanici == 3:
+        break
+
+if skor_bilgisayar > skor_kullanici:
+    print("KAYBETTİNİZ")
+else:
+    print("KAZANDINIZ")
+
+
+
